29edbed9192acc748a3306abf5d9ea360c06da34
JohannesHamann <JohannesHamann1@gmail.com>
2020-03-20 17:27:25 +0100

    first commit
    copying of the existing local files of my python project

---
+++ first_website.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+from flask import Flask, render_template, request
+
+# __name__ ist der Eigenname der Datei also "first_website_server.py". Theoretisch könnnen ja auch externe Module gelesen werden. 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+    out =  render_template("first_flask_project.html", name="Johannes Hamann")
+    return out
+@app.route("/test")
+def test():
+    args = request.args
+    name = request.args.get("name")
+    alter = request.args.get("age")
+    if name != None and alter != None:
+        print name + " ist " + alter + " Jahre alt."
+    return render_template("test.html", name= name)
+
+@app.route("/formular")
+def formular():
+    name = request.args.get("name")
+    alter = request.args.get("age")
+    print name, alter
+    return render_template("formular.html", name = name, age = alter)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
